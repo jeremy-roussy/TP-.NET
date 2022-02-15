09f3bfd6e698133ee833b1f3c71713420b5db371
Paul-Kenji <p.bochaton@net.estia.fr>
2022-02-15 14:06:58 +0100

    ajout nouvelle branche à merge desuite base projet pour tout le monde

---
+++ ASP.Server/Models/GenreFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class GenreFilter
+{
+	public GenreFilter()
+	{
+		public Genre Genre { init; private get; }
+		public string Type { get => Genre.ToString() }
+		public List<BookLite> Books { get => Genre.Books.Select(x => new BookLite() { Books = x }).ToList(); }
+	}
+}
