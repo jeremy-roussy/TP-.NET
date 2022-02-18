17fb4ba233680fe3f6156374f312ab5c6c31a9b5
Antoine DERIAN <a.derian@net.estia.fr>
2022-02-18 08:50:04 +0100

    Début WPF

--- WPF.Reader/ViewModel/DetailsBook.cs
+++ WPF.Reader/ViewModel/DetailsBook.cs
@@ -1,13 +1,15 @@
-﻿using System.ComponentModel;
+﻿using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using System.ComponentModel;
 using System.Windows.Input;
 using WPF.Reader.Model;
+using WPF.Reader.Service;
 
 namespace WPF.Reader.ViewModel
 {
     public class DetailsBook : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        public ICommand ReadCommand { get; init; } = new RelayCommand(x => { /* A vous de définir la commande */ });
+        public ICommand ReadCommand { get; init; } = new RelayCommand(x => {  });
 
         // n'oublier pas faire de faire le binding dans DetailsBook.xaml !!!!
         public Book CurrentBook { get; init; }
@@ -15,6 +17,11 @@ namespace WPF.Reader.ViewModel
         public DetailsBook(Book book)
         {
             CurrentBook = book;
+        }
+
+        public DetailsBook()
+        {
+            ReadCommand = new RelayCommand(book => { Ioc.Default.GetRequiredService<INavigationService>().Navigate<ReadBook>(CurrentBook); });
         }
     }
 
